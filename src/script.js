02d02979f8852330f087abd8b02ab980fb1d0442
Felipe <felipe.baleche2@gmail.com>
2024-01-08 11:00:46 -0300

    Add book form functionality and library array

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+const library = []
+
 function Book(title, author, pages, read){
 
     //Attributes
@@ -10,8 +12,18 @@ function Book(title, author, pages, read){
     this.info = () => {
         return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`;
     }
+
 }
 
 function openDialog(){
     document.querySelector('.add-book-container').showModal();
+}
+
+function addBookToLibrary(){
+    const form = document.querySelector('.book-form')
+
+    let formData = new FormData(form)
+    let data = Object.fromEntries(formData)
+
+    library.push(new Book(data.title, data.author, data.pages, data.read))
 }
