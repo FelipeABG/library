56b79703f55c7f009242e5a97e4d083c2361c804
Felipe <felipe.baleche2@gmail.com>
2024-01-09 10:42:01 -0300

    Update submit button functionality and display books in the library

--- src/script.js
+++ src/script.js
@@ -15,6 +15,15 @@ function Book(title, author, pages, read){
 
 }
 
+Book.prototype.toggleRead = () => {
+    if(this.read){
+        this.read = false;
+    }
+    else{
+        this.read = true;
+    }
+}
+
 function openDialog(){
     document.querySelector('.add-book-container').showModal();
 }
@@ -25,9 +34,33 @@ function addBookToLibrary(){
     let formData = new FormData(form)
     let data = Object.fromEntries(formData)
 
-    library.push(new Book(data.title, data.author, data.pages, data.read))
+    library.push(new Book(data.title, data.author, data.pages, data.read === "on" ? true : false))
 }
 
 function displayBooks(){
-    
+    const container = document.querySelector('.card-container')
+
+    container.innerHTML = ''
+    library.forEach((book) => {
+        container.innerHTML += 
+        `
+            <div class="card">
+                <div class="title">
+                    <h2>${book.title}</h2>
+                </div>
+                <div class="author">
+                    <h3>Author:</h3>
+                    <p>${book.author}</p>
+                </div>
+                <div class="page-number">
+                    <h3>Pages:</h3>
+                    <p>${book.pages}</p>
+                </div>
+                <div class="buttons">
+                    <button class="read-button">Read</button>
+                    <button class="delete-button">Delete</button>
+                </div>
+            </div>
+        `
+    })
 }
